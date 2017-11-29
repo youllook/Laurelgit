5ff0e4d656560d10dcec59cb9f5a6f87d34ff797
tony <tonylu@esobi.com>
2017-11-30 06:14:58 +0800

    補檔案 - SettingFile

---
+++ Laurel_game/Library/SettingFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Laurel_game.Models;
+using Newtonsoft.Json;
+
+namespace Laurel_game.Library
+{
+    public class SettingFile
+    {
+        public static CostSettingModel GetCostSet()
+        {
+            //Get CostData
+            CostSettingModel costSet = new CostSettingModel();
+            string path = AppDomain.CurrentDomain.BaseDirectory + "Files/cost_setting.json";
+            string content = Library.FileLib.ReadFile(path);
+            costSet = JsonConvert.DeserializeObject<CostSettingModel>(content);
+            return costSet;
+        }
+    }
+}
